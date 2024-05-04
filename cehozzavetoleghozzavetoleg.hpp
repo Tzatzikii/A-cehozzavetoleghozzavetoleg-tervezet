4715fb729abc154cbac8e58d7563eb16f15663ff
Gabor Klemm <klemm.gabor.14@gmail.com>
2024-05-04 20:08:51 +0200

    elso elkotelezes

---
+++ cehozzavetoleghozzavetoleg.hpp
@@ -0,0 +1,41 @@
+#ifndef CEHOZZAVETOLEGHOZZAVETOLEG_HPP_
+#define CEHOZZAVETOLEGHOZZAVETOLEG_HPP_
+
+#define megszuletik new
+#define meghal delete
+#define allando const
+#define valtozekony mutable
+#define nyugvo static
+#define kivuleso extern
+#define allando_kifejezmeny constexpr
+#define illo volatile
+#define sorkozi inline // vagy "beszurt" ?
+#define nemnegativ unsigned
+#define motorgepjarmu auto
+#define eldontendo bool
+#define egesz int
+#define lebego float // vagy "pontatlan"?
+#define kettos double // vagy "pontos"?
+#define	felsorolas enum
+#define szerkezet struct
+#define osztaly class
+#define foka sizeof
+#define dinamikus_tipusontes dynamic_cast
+#define	allando_tipusontes const_cast
+#define erte for
+#define amig while
+#define hogyha if
+#define (kulonben ha) else if
+#define (es ha semelyik) else
+#define valtogatas switch
+#define alapertelmezett default
+#define nemvalos virtual
+#define csakakkoris override
+#define kifejezett explicit
+#define kivetelnelkuli noexcept
+#define menjoda goto
+#define	(probald meg) try
+#define (kapd el) catch
+#define (hajitsd el) throw
+
+#endif
